19b4277719185501f1a8e134ef720ad815cc644d
dbacall <dbacall@hotmail.co.uk>
2021-02-19 09:36:01 +0000

    add conditional

--- src/components/Category/CategoryComponent.js
+++ src/components/Category/CategoryComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Category.module.scss';
 import { Link } from 'react-router-dom';
 import Loader from '../commons/Loader';
+import Navbar from '../navbar/navbarContainer';
 
 const Category = ({ therapists = [], status, category }) => {
   const renderTherapists = () => {
@@ -15,18 +16,20 @@ const Category = ({ therapists = [], status, category }) => {
             className={styles.therapist}
             key={index}
           >
-            {therapist.photo.length > 0 ? (
-              <div className={styles.imageContainer}>
+            <div className={styles.imageContainer}>
+              {therapist.photo.length > 0 && (
                 <img
                   className={styles.therapistImage}
                   src={`${process.env.REACT_APP_S3_BUCKET_URL}/${therapist.photo[0].photo}`}
                   alt={`${therapist.user[0].firstName} ${therapist.user[0].surname}`}
                 ></img>
-              </div>
-            ) : null}
-            <h4 className={styles.therapistName}>
-              {therapist.user[0].firstName} {therapist.user[0].surname}
-            </h4>
+              )}
+            </div>
+            {therapist.user.length && (
+              <h4 className={styles.therapistName}>
+                {therapist.user[0].firstName} {therapist.user[0].surname}
+              </h4>
+            )}
           </Link>
         );
       });
@@ -34,9 +37,12 @@ const Category = ({ therapists = [], status, category }) => {
   };
 
   return (
-    <div className={styles.category}>
-      <h2 className={styles.title}>Therapists</h2>
-      <div className={styles.therapists}>{renderTherapists()}</div>
+    <div>
+      <Navbar />
+      <div className={styles.category}>
+        <h2 className={styles.title}>Therapists</h2>
+        <div className={styles.therapists}>{renderTherapists()}</div>
+      </div>
     </div>
   );
 };
